8daa515e1827ded4bed9f46fcf5e36715d8efd56
Tomoya Koyanagi <tomk79@gmail.com>
2022-10-23 23:39:06 +0900

    管理画面のデザインを刷新

--- src/cloverMain/layouts/Root.jsx
+++ src/cloverMain/layouts/Root.jsx
@@ -18,26 +18,23 @@ export default React.memo(function Root(props){
 							<span className="theme-layout__title-pj-name">
 								<Link href={main.px2utils.href('/?PX=admin')}>{(window.px2config.name)}</Link>
 							</span>
-							<span className="theme-layout__title-console-name">
-								Admin Console
-							</span>
 						</div>
+						<nav className="theme-layout__left-navbar">
+							<ul>
+								<li><Link href="/?PX=admin.sitemap">サイトマップ</Link></li>
+								<li><Link href="/?PX=admin.page_info">ページ情報</Link></li>
+								<li><Link href="/?PX=admin.theme">テーマ</Link></li>
+								<li><Link href="/?PX=admin.modules">モジュール</Link></li>
+								<li><Link href="/?PX=admin.publish">パブリッシュ</Link></li>
+								<li><Link href="/?PX=admin.history">履歴</Link></li>
+								<li><Link href="/?PX=admin.clearcache">キャッシュを消去</Link></li>
+								<li><Link href="/?PX=admin.config">設定</Link></li>
+							</ul>
+						</nav>
 					</div>
 				</header>
 
 				<div className="theme-layout__main">
-					<nav className="theme-layout__left-navbar">
-						<ul>
-							<li><Link href="/?PX=admin.sitemap">サイトマップ</Link></li>
-							<li><Link href="/?PX=admin.page_info">ページ情報</Link></li>
-							<li><Link href="/?PX=admin.theme">テーマ</Link></li>
-							<li><Link href="/?PX=admin.modules">モジュール</Link></li>
-							<li><Link href="/?PX=admin.publish">パブリッシュ</Link></li>
-							<li><Link href="/?PX=admin.history">履歴</Link></li>
-							<li><Link href="/?PX=admin.clearcache">キャッシュを消去</Link></li>
-							<li><Link href="/?PX=admin.config">設定</Link></li>
-						</ul>
-					</nav>
 					<div className="theme-layout__main-center">
 						<div className="theme-layout__main-center-h1">
 							<h1>{props.title}</h1>
@@ -48,18 +45,19 @@ export default React.memo(function Root(props){
 							</main>
 						</div>
 					</div>
+
+					<footer className="theme-layout__footer">
+						{(main.profile && main.profile.name)
+							? <>
+								<div className="px2-text-align-center">Logged in as: {main.profile.name}</div>
+							</>
+							: <></>}
+						<ul>
+							<li><a href="?">プレビューへ戻る</a></li>
+							<li><a href="?PX=admin.logout">ログアウト</a></li>
+						</ul>
+					</footer>
 				</div>
-				<footer className="theme-layout__footer">
-					{(main.profile && main.profile.name)
-						? <>
-							<div className="px2-text-align-center">Logged in as: {main.profile.name}</div>
-						</>
-						: <></>}
-					<ul>
-						<li><a href="?">プレビューへ戻る</a></li>
-						<li><a href="?PX=admin.logout">ログアウト</a></li>
-					</ul>
-				</footer>
 			</div>
 		</>
 	);
