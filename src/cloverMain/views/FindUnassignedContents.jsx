8a8158c4f598c2584ff550e5a87747dce1ffc479
Tomoya Koyanagi <tomk79@gmail.com>
2022-12-25 01:11:25 +0900

    未アサインコンテンツの一覧から、コンテンツを削除できるようになった。

--- src/cloverMain/views/FindUnassignedContents.jsx
+++ src/cloverMain/views/FindUnassignedContents.jsx
@@ -11,9 +11,9 @@ export default function FindUnassignedContents(props){
 	/**
 	 * 未アサインコンテンツの検索結果を取得する
 	 */
-	function findUnassignedContents(e){
+	function getUnassignedContentList(callback){
+		callback = callback || function(){};
 		px2style.loading();
-		$(e.target).attr({'disabled':'disabled'});
 		main.px2utils.px2cmd(
 			"?PX=px2dthelper.get.list_unassigned_contents",
 			{},
@@ -33,28 +33,76 @@ export default function FindUnassignedContents(props){
 				}
 
 				px2style.closeLoading();
-				$(e.target).removeAttr('disabled');
 
 				setUnassignedContentsList(data.unassigned_contents);
+
+				callback();
+			}
+		);
+	}
+
+	/**
+	 * 未アサインコンテンツを削除する
+	 */
+	function deleteContent( target_path ){
+		if(!confirm('このコンテンツ '+target_path+' を削除しますか？')){
+			return;
+		}
+
+		main.px2utils.px2cmd(
+			target_path + '?PX=px2dthelper.content.delete',
+			{},
+			function( res ){
+				console.log(res);
+				if( !res.result ){
+					alert( 'Error: ' + res.message );
+					console.error('Error:', res);
+					return;
+				}
+				getUnassignedContentList();
 			}
 		);
 	}
 
 	return (
 		<>
-			<div>
-				<button className="px2-btn px2-btn--primary" onClick={findUnassignedContents}>未アサインコンテンツを検索する</button>
+			<div className="px2-p">
+				<button className="px2-btn px2-btn--primary" onClick={(e)=>{
+					$(e.target).attr({'disabled':'disabled'});
+					getUnassignedContentList(function(){
+						$(e.target).removeAttr('disabled');
+					});
+				}}>未アサインコンテンツを検索する</button>
 			</div>
-			{unassignedContentsList!==null && unassignedContentsList.length && (
+			{(unassignedContentsList!==null
+				?
 				<>
-					<ul>
-					{(unassignedContentsList.map((unassignedContent)=>{
-						return (<>
-							<li>{unassignedContent}</li>
-						</>);
-					}))}
-					</ul>
+					{(unassignedContentsList.length
+						?
+						<>
+							<table className="px2-table" style={{widht:"100%"}}>
+							{(unassignedContentsList.map((unassignedContent, idx)=>{
+								return (
+									<tr key={idx}>
+										<th>{unassignedContent}</th>
+										<td><button type="button" data-target-content={unassignedContent} onClick={(e)=>{
+											var target_path = $(e.target).attr('data-target-content');
+											target_path = target_path.replace(/(\.html?)(\.[a-zA-Z0-9]+)?$/, '$1');
+											deleteContent(target_path);
+										}} className="px2-btn px2-btn--danger">remove</button></td>
+									</tr>
+								);
+							}))}
+							</table>
+						</>
+						:
+						<>
+							<p>未アサインコンテンツは検出されませんでした。</p>
+						</>
+					)}
 				</>
+				:
+				<></>
 			)}
 		</>
 	);
