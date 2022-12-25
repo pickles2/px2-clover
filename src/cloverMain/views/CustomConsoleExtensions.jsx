cb2ece7c002e6a963e8b241c9771dad1080b040f
Tomoya Koyanagi <tomk79@gmail.com>
2022-12-25 23:51:30 +0900

    <CCE> 初期化処理の改善

--- src/cloverMain/views/CustomConsoleExtensions.jsx
+++ src/cloverMain/views/CustomConsoleExtensions.jsx
@@ -8,7 +8,6 @@ export default function CustomConsoleExtensions(props){
 	const main = useContext(MainContext);
 	const path_client_resources = window.clover_config.paths.path_client_resources;
 	const [customConsoleExtensionsList, setCustomConsoleExtensionsList] = useState(null);
-	let px2dthelperCceAgent;
 
 	useEffect(() => {
 		main.px2utils.px2cmd(
@@ -42,6 +41,136 @@ export default function CustomConsoleExtensions(props){
 		return customConsoleExtensionsList.list[currentCceId];
 	})();
 
+	/**
+	 * CCEを初期化する
+	 */
+	function initializeCce(){
+		let cceAgent;
+
+		iterate79.fnc({}, [
+			function(it1){
+				if( main.title != currentCce.label ){
+					main.setMainState({
+						title: currentCce.label,
+					});
+					return;
+				}
+				it1.next();
+			},
+			function(it1){
+				main.px2utils.px2cmd(
+					`/?PX=px2dthelper.custom_console_extensions.${currentCceId}.client_resources`,
+					{},
+					function( res ){
+						if( !res.result ){
+							alert('Undefined Extension. ' + res.message);
+							return;
+						}
+						var resources = res.resources;
+
+						iterate79.ary(
+							resources.css,
+							function(it2, row, idx){
+								var link = document.createElement('link');
+								link.addEventListener('load', function(){
+									it2.next();
+								});
+								$('.cont-custom-console-extensions__resources').append(link);
+								link.rel = 'stylesheet';
+								link.href = path_client_resources+'__cce/'+currentCceId+'/'+row;
+							},
+							function(){
+								iterate79.ary(
+									resources.js,
+									function(it3, row, idx){
+										var script = document.createElement('script');
+										script.addEventListener('load', function(){
+											it3.next();
+										});
+										$('.cont-custom-console-extensions__resources').append(script);
+										script.src = path_client_resources+'__cce/'+currentCceId+'/'+row;
+									},
+									function(){
+										it1.next();
+									}
+								);
+							}
+						);
+
+					}
+				);
+			},
+			function(it1){
+				cceAgent = new window.Px2dthelperCceAgent({
+					'elm': $('.cont-custom-console-extensions__main').get(0),
+					'lang': main.lb.getLang(),
+					'appMode': 'web',
+					'gpiBridge': function(input, callback){
+						// GPI(General Purpose Interface) Bridge
+
+						console.info('TODO: CCE: gpiBridge called;');
+
+						// var getParam = '';
+						// getParam += 'PX=px2dthelper.custom_console_extensions.'+customConsoleExtensionId+'.gpi'
+						// 	+'&request='+encodeURIComponent( JSON.stringify(input) )
+						// 	+'&appMode=desktop'
+						// 	+'&asyncMethod=file'
+						// 	+'&asyncDir='+watchDir+'async/'+pj.projectInfo.id+'/'
+						// 	+'&broadcastMethod=file'
+						// 	+'&broadcastDir='+watchDir+'broadcast/'+pj.projectInfo.id+'/';
+						// // console.log(getParam);
+
+						// var testTimestamp = (new Date()).getTime();
+						// var tmpFileName = '__tmp_'+main.utils79.md5( Date.now() )+'.json';
+						// // console.log('=-=-=-=-=-=-=-=', realpathDataDir+tmpFileName, getParam);
+						// main.fs.writeFileSync( realpathDataDir+tmpFileName, getParam );
+
+						// pj.execPx2(
+						// 	'/?' + getParam,
+						// 	{
+						// 		'method': 'post',
+						// 		'bodyFile': tmpFileName,
+						// 		'complete': function(rtn){
+						// 			// console.log('--- returned(millisec)', (new Date()).getTime() - testTimestamp);
+						// 			new Promise(function(rlv){rlv();})
+						// 				.then(function(){ return new Promise(function(rlv, rjt){
+						// 					try{
+						// 						rtn = JSON.parse(rtn);
+						// 					}catch(e){
+						// 						console.error('Failed to parse JSON String -> ' + rtn);
+						// 					}
+						// 					rlv();
+						// 				}); })
+						// 				.then(function(){ return new Promise(function(rlv, rjt){
+						// 					main.fs.unlinkSync( realpathDataDir+tmpFileName );
+						// 					rlv();
+						// 				}); })
+						// 				.then(function(){ return new Promise(function(rlv, rjt){
+						// 					callback( rtn );
+						// 				}); })
+						// 			;
+						// 		}
+						// 	}
+						// );
+						return;
+					},
+				});
+
+				it1.next();
+			},
+			function(it1){
+				// --------------------------------------
+				// Custom Console Extension の初期化を実行する
+				eval(currentCce.client_initialize_function+'(px2dthelperCceAgent);');
+				it1.next();
+			},
+			function(){
+			},
+		]);
+		return;
+	}
+
+
 	return (
 		<>
 			{(customConsoleExtensionsList === null)
@@ -51,141 +180,37 @@ export default function CustomConsoleExtensions(props){
 				: <>
 					{currentCceId === null
 						? <>
-							<div className="px2-linklist">
-								<ul>
-									{(Object.keys(customConsoleExtensionsList.list).map((cceKey, index) => {
-										const listItem = customConsoleExtensionsList.list[cceKey];
-										return <li key={index}>
-											<Link href={`?PX=admin.cce.${listItem.id}`}>{ listItem.label }</Link>
-										</li>;
-									}))}
-								</ul>
-							</div>
+							{Object.keys(customConsoleExtensionsList.list).length
+								? <>
+									<div className="px2-linklist">
+										<ul>
+											{(Object.keys(customConsoleExtensionsList.list).map((cceKey, index) => {
+												const listItem = customConsoleExtensionsList.list[cceKey];
+												return <li key={index}>
+													<Link href={`?PX=admin.cce.${listItem.id}`}>{ listItem.label }</Link>
+												</li>;
+											}))}
+										</ul>
+									</div>
+								</>
+								: <>
+									<p>拡張機能は登録されていません。</p>
+								</>
+							}
 							<div className="px2-p">
 								<Link href="?PX=admin.config">戻る</Link>
 							</div>
 						</>
 						: <>
-							<p>{currentCce.label}</p>
-							<div className="cont-custom-console-extensions-main"></div>
+							<div className="cont-custom-console-extensions">
+								<div className="cont-custom-console-extensions__main"></div>
+								<div className="cont-custom-console-extensions__resources"></div>
+							</div>
 							<div className="px2-p">
 								<Link href="?PX=admin.cce">戻る</Link>
 							</div>
-							<div className="cont-custom-console-extensions-resources"></div>
 							{(()=>{
-								iterate79.fnc({}, [
-									function(it1){
-										main.px2utils.px2cmd(
-											`/?PX=px2dthelper.custom_console_extensions.${currentCceId}.client_resources`,
-											{},
-											function( res ){
-												if( !res.result ){
-													alert('Undefined Extension. ' + res.message);
-													return;
-												}
-												var resources = res.resources;
-
-												iterate79.ary(
-													resources.css,
-													function(it2, row, idx){
-														var link = document.createElement('link');
-														link.addEventListener('load', function(){
-															it2.next();
-														});
-														$('.cont-custom-console-extensions-resources').append(link);
-														link.rel = 'stylesheet';
-														link.href = path_client_resources+'__cce/'+currentCceId+'/'+row;
-													},
-													function(){
-														iterate79.ary(
-															resources.js,
-															function(it3, row, idx){
-																var script = document.createElement('script');
-																script.addEventListener('load', function(){
-																	it3.next();
-																});
-																$('.cont-custom-console-extensions-resources').append(script);
-																script.src = path_client_resources+'__cce/'+currentCceId+'/'+row;
-															},
-															function(){
-																it1.next();
-															}
-														);
-													}
-												);
-
-											}
-										);
-									},
-									function(it1){
-
-										px2dthelperCceAgent = new Px2dthelperCceAgent({
-											'elm': $('.cont-custom-console-extensions-main').get(0),
-											'lang': main.lb.getLang(),
-											'appMode': 'web',
-											'gpiBridge': function(input, callback){
-												// GPI(General Purpose Interface) Bridge
-console.info('TODO: CCE: gpiBridge called.');
-
-												// var getParam = '';
-												// getParam += 'PX=px2dthelper.custom_console_extensions.'+customConsoleExtensionId+'.gpi'
-												// 	+'&request='+encodeURIComponent( JSON.stringify(input) )
-												// 	+'&appMode=desktop'
-												// 	+'&asyncMethod=file'
-												// 	+'&asyncDir='+watchDir+'async/'+pj.projectInfo.id+'/'
-												// 	+'&broadcastMethod=file'
-												// 	+'&broadcastDir='+watchDir+'broadcast/'+pj.projectInfo.id+'/';
-												// // console.log(getParam);
-
-												// var testTimestamp = (new Date()).getTime();
-												// var tmpFileName = '__tmp_'+main.utils79.md5( Date.now() )+'.json';
-												// // console.log('=-=-=-=-=-=-=-=', realpathDataDir+tmpFileName, getParam);
-												// main.fs.writeFileSync( realpathDataDir+tmpFileName, getParam );
-
-												// pj.execPx2(
-												// 	'/?' + getParam,
-												// 	{
-												// 		'method': 'post',
-												// 		'bodyFile': tmpFileName,
-												// 		'complete': function(rtn){
-												// 			// console.log('--- returned(millisec)', (new Date()).getTime() - testTimestamp);
-												// 			new Promise(function(rlv){rlv();})
-												// 				.then(function(){ return new Promise(function(rlv, rjt){
-												// 					try{
-												// 						rtn = JSON.parse(rtn);
-												// 					}catch(e){
-												// 						console.error('Failed to parse JSON String -> ' + rtn);
-												// 					}
-												// 					rlv();
-												// 				}); })
-												// 				.then(function(){ return new Promise(function(rlv, rjt){
-												// 					main.fs.unlinkSync( realpathDataDir+tmpFileName );
-												// 					rlv();
-												// 				}); })
-												// 				.then(function(){ return new Promise(function(rlv, rjt){
-												// 					callback( rtn );
-												// 				}); })
-												// 			;
-												// 		}
-												// 	}
-												// );
-												return;
-											},
-										});
-										// pj.onCceBroadcast(function(message){
-										// 	px2dthelperCceAgent.putBroadcastMessage(message);
-										// });
-
-										it1.next();
-									},
-									function(it1){
-										eval(currentCce.client_initialize_function+'(px2dthelperCceAgent);');
-										it1.next();
-									},
-									function(it1){
-										console.log('=-=-=-=-= done.');
-									},
-								]);
+								initializeCce();
 								return <></>;
 							})()}
 						</>
