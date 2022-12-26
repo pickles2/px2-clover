65f5f7ae50997cda90ecbc08a6192fa6f51ddb98
Tomoya Koyanagi <tomk79@gmail.com>
2022-12-26 12:54:09 +0900

    <CCE> async, broadcast のコマンドファイルを _sys/ram/data に保存するように変更した

--- php/register.php
+++ php/register.php
@@ -125,9 +125,9 @@ class register{
 			if( preg_match('/^px2dthelper\.custom_console_extensions\.([a-zA-Z0-9\_\-]+)\.gpi$/', ''.$px->req()->get_param('PX'), $matched) ){
 				// Custom Console Extension のGPI呼び出しに伴い、async, broadcast コマンドの保存先を書き換える
 				$px->req()->set_param('appMode', 'web');
-				$client_resources_dist = $px->realpath_plugin_files('/');
-				$realpath_async_dir = $px->fs()->normalize_path($px->fs()->get_realpath( $client_resources_dist.'../px2-clover/__cce_async/'.$matched[1].'/' ));
-				$realpath_broadcast_dir = $px->fs()->normalize_path($px->fs()->get_realpath( $client_resources_dist.'../px2-clover/__cce_broadcast/'.$matched[1].'/' ));
+				$watcher_basedir = $px->get_realpath_homedir().'_sys/ram/data/px2-clover/cce/';
+				$realpath_async_dir = $px->fs()->normalize_path($px->fs()->get_realpath( $watcher_basedir.'async/'.$matched[1].'/' ));
+				$realpath_broadcast_dir = $px->fs()->normalize_path($px->fs()->get_realpath( $watcher_basedir.'broadcast/'.$matched[1].'/' ));
 				$px->fs()->mkdir_r($realpath_async_dir);
 				$px->req()->set_param('asyncMethod', 'file');
 				$px->req()->set_param('asyncDir', $realpath_async_dir);
