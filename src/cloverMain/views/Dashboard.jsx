26312483a747b79cfaafd259f133c916ec716c41
Tomoya Koyanagi <tomk79@gmail.com>
2023-01-03 14:30:26 +0900

    <editSiteProfile> 編集モーダルを追加

--- src/cloverMain/views/Dashboard.jsx
+++ src/cloverMain/views/Dashboard.jsx
@@ -6,6 +6,54 @@ import iterate79 from 'iterate79';
 export default function Dashboard(props){
 
 	const main = useContext(MainContext);
+	const templates = {
+		"editSiteProfile": require('./Dashboard_files/templates/editSiteProfile.twig'),
+	};
+
+	/**
+	 * サイト設定情報を編集する
+	 */
+	function editSiteProfile(){
+		iterate79.fnc({}, [
+			function(it1){
+				// TODO: 編集可能な項目を確認する
+				it1.next();
+			},
+			function(it1){
+				const $body = $(templates.editSiteProfile({
+					"name": main.pxConfig.name,
+					"domain": main.pxConfig.domain,
+					"copyright": main.pxConfig.copyright,
+				}));
+				px2style.modal({
+					"title": "Edit Site Profile",
+					"body": $body,
+					"form": {
+						"action": "javascript:void(0);",
+						"method": "get",
+						"submit": function(){
+							// TODO: フォームの編集内容を保存する
+							alert('Form has submitted.');
+						},
+					},
+					'buttons':[
+						$('<button type="submit" class="px2-btn px2-btn--primary">')
+							.text('保存する')
+					],
+					'buttonsSecondary': [
+						$('<button type="button" class="px2-btn">')
+							.text('キャンセル')
+							.on('click', function(){
+								px2style.closeModal();
+							}),
+					],
+				});
+
+				it1.next();
+			},
+		]);
+	}
+
 
 	return (
 		<>
@@ -34,6 +82,9 @@ export default function Dashboard(props){
 						</tr>
 						</tbody>
 					</table>
+					<p>
+						<button type="button" className="px2-btn px2-btn--primary" onClick={editSiteProfile}>編集する</button>
+					</p>
 				</>
 			}
 		</>
