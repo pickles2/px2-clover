5c2c6150bddaa6dc7f166790f91c8d031b2e647e
Tomoya Koyanagi <tomk79@gmail.com>
2023-04-11 00:58:07 +0900

    設定ページの構成に見出しを追加

--- src/cloverMain/views/Config.jsx
+++ src/cloverMain/views/Config.jsx
@@ -8,19 +8,35 @@ export default function Config(props){
 
 	return (
 		<>
+			<h2>アカウント</h2>
 			<div className="px2-linklist">
 				<ul>
 					<li><Link href="?PX=admin.config.profile">{main.lb.get('page_title.config_profile')}</Link></li>
 					<li><Link href="?PX=admin.config.members">{main.lb.get('page_title.config_members')}</Link></li>
+				</ul>
+			</div>
+			<h2>環境設定</h2>
+			<div className="px2-linklist">
+				<ul>
 					<li><Link href="?PX=admin.config.history">{main.lb.get('page_title.config_history')}</Link></li>
 					<li><Link href="?PX=admin.config.scheduler">{main.lb.get('page_title.config_scheduler')}</Link></li>
 					<li><Link href="?PX=admin.config.maintenance">{main.lb.get('page_title.config_maintenance')}</Link></li>
+				</ul>
+			</div>
+			<h2>ユーティリティ</h2>
+			<div className="px2-linklist">
+				<ul>
 					<li><Link href="?PX=admin.sitemap">{main.lb.get('page_title.sitemap')}</Link></li>
-					<li><Link href="?PX=admin.config.find_unassigned_contents">{main.lb.get('page_title.config_find_unassigned_contents')}</Link></li>
 					<li><Link href="?PX=admin.modules">{main.lb.get('page_title.modules')}</Link></li>
+					<li><Link href="?PX=admin.config.find_unassigned_contents">{main.lb.get('page_title.config_find_unassigned_contents')}</Link></li>
 					<li><Link href="?PX=admin.config.files_and_folders">{main.lb.get('page_title.config_files_and_folders')}</Link></li>
-					<li><Link href="?PX=admin.cce">{main.lb.get('page_title.cce')}</Link></li>
 					<li><Link href="?PX=admin.clearcache">{main.lb.get('page_title.clearcache')}</Link></li>
+				</ul>
+			</div>
+			<h2>拡張機能</h2>
+			<div className="px2-linklist">
+				<ul>
+					<li><Link href="?PX=admin.cce">{main.lb.get('page_title.cce')}</Link></li>
 				</ul>
 			</div>
 		</>
